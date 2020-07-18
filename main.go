e848ec6afed4fd257432585e60ca1663b73b99d4
Ratnadeep Bhattcharyya <ratnadeep.bhattacharyya@cura.tech>
2020-07-18 13:55:18 +0530

    added getting token for specific provider

--- main.go
+++ main.go
@@ -23,11 +23,36 @@ func main() {
 		fmt.Println(err.Error())
 		os.Exit(0)
 	}
+	if len(os.Args) > 1 {
+		providerName := os.Args[1]
+		if providerName != "" {
+			envs := strings.Split(string(envText), "\n")
+			for _, env := range envs {
+				envSplit := strings.Split(env, "=")
+				secret := envSplit[1]
+				provider := envSplit[0]
+				if strings.ToLower(provider) == strings.ToLower(providerName) {
+					token := getToken(string(secret))
+					printToken(string(envSplit[0]), token)
+				}
+			}
+			return
+		}
+	}
 	envs := strings.Split(string(envText), "\n")
 	for _, env := range envs {
 		envSplit := strings.Split(env, "=")
 		secret := envSplit[1]
-		token, _ := totp.GenerateCode(string(secret), time.Now())
-		fmt.Printf("%s: %s\n", string(envSplit[0]), token)
+		token := getToken(string(secret))
+		printToken(string(envSplit[0]), token)
 	}
+}
+
+func getToken(secret string) string {
+	token, _ := totp.GenerateCode(secret, time.Now())
+	return token
+}
+
+func printToken(provider string, token string) {
+	fmt.Printf("%s: %s\n", provider, token)
 }
